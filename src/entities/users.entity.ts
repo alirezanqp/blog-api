d5663acb1812dc6502654cffd25a90397088ec53
GitLab <test@example.com>
2022-07-01 20:33:40 +0430

    add and update comments, posts, users entities

--- src/entities/users.entity.ts
+++ src/entities/users.entity.ts
@@ -1,6 +1,10 @@
 import { IsNotEmpty } from 'class-validator';
-import { BaseEntity, Entity, PrimaryGeneratedColumn, Column, Unique, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+import { BaseEntity, Entity, PrimaryGeneratedColumn, Column, Unique, CreateDateColumn, UpdateDateColumn, OneToMany } from 'typeorm';
 import { User } from '@interfaces/users.interface';
+import { Post } from '@interfaces/posts.interface';
+import { PostEntity } from '@entities/post.entity';
+import { Comment } from '@interfaces/comments.interface';
+import { CommentEntity } from '@entities/comment.entity';
 
 @Entity()
 export class UserEntity extends BaseEntity implements User {
@@ -21,11 +25,17 @@ export class UserEntity extends BaseEntity implements User {
   name: string;
 
   @Column()
-  lastname: string;
+  lastName: string;
 
   @Column()
   @IsNotEmpty()
   password: string;
+
+  @OneToMany(() => PostEntity, post => post.user)
+  posts: Post[];
+
+  @OneToMany(() => CommentEntity, comment => comment.user)
+  comments: Comment[];
 
   @Column()
   @CreateDateColumn()
