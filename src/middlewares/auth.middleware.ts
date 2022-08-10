3a32c204b64c1c4b567b61436d15abe617848a30
GitLab <test@example.com>
2022-08-10 21:28:44 +0430

    edit auth middleware messages

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -18,13 +18,13 @@ const authMiddleware = async (req: RequestWithUser, res: Response, next: NextFun
         req.user = findUser;
         next();
       } else {
-        next(new HttpException(401, 'Wrong authentication token'));
+        next(new HttpException(401, 'توکن معتبر نیست'));
       }
     } else {
-      next(new HttpException(404, 'Authentication token missing'));
+      next(new HttpException(404, 'اعتبار سنجی ناموفق'));
     }
   } catch (error) {
-    next(new HttpException(401, 'Wrong authentication token'));
+    next(new HttpException(401, 'توکن معتبر نیست'));
   }
 };
 
